564bcdda72962b0c4b8cee3e43435bc3540ef72c
Rajeev Tapadia <rtapadia730@gmail.com>
2024-03-17 19:47:49 +0530

    showing albums on artist dashboard

--- router/artistDash.js
+++ router/artistDash.js
@@ -3,8 +3,17 @@ import { connection } from "../index.js";
 
 const artistDash = Router();
 
-artistDash.get("/:id", (req, res) => {
-  res.render("artist-dashboard.hbs");
+artistDash.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const [[artist]] = await connection.query(
+    "SELECT * FROM artists WHERE id = ?",
+    id
+  );
+  const [albums] = await connection.query(
+    "SELECT * FROM albums WHERE artist_id = ?",
+    id
+  );
+  res.render("artist-dashboard.hbs", {albums, artist});
 });
 
 export default artistDash;
