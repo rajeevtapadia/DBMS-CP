d8f57861648daa6ae9ec2b50cc5698606926e268
Rajeev Tapadia <rtapadia730@gmail.com>
2024-03-20 22:35:52 +0530

    songs table crud

--- router/artistDash.js
+++ router/artistDash.js
@@ -13,7 +13,35 @@ artistDash.get("/:id", async (req, res) => {
     "SELECT * FROM albums WHERE artist_id = ?",
     id
   );
-  res.render("artist-dashboard.hbs", { albums, artist });
+  const [allSongs] = await connection.query(
+    `SELECT * from songs where artist_id = ?`,
+    id
+  );
+  res.render("artist-dashboard.hbs", { albums, artist, allSongs });
+});
+
+// add a song to songs table
+artistDash.post("/:id/song/add", (req, res) => {
+  const { title, duration } = req.body;
+  const { id } = req.params;
+
+  connection
+    .query(
+      `INSERT INTO songs(title, duration, artist_id)
+      VALUES (?, ?, ?)`,
+      [title, duration, id]
+    )
+    .then(() => {
+      res.redirect("../");
+    });
+});
+
+// delete a song
+artistDash.post("/:artistId/song/delete/:songId", (req, res) => {
+  const { artistId, songId } = req.params;
+  connection.query(`DELETE FROM songs WHERE id = ?`, songId).then(() => {
+    res.redirect("../../");
+  });
 });
 
 export default artistDash;
